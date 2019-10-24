2520a3c7be6f59f62e9969ceec30b33f281ab317
Norbert Guda <norbertguda94@gmail.com>
2019-10-24 22:34:53 +0300

    adding object property

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -137,7 +137,9 @@ describe("basics", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            const object = {
+                someProperty: 1
+            }
             ////////////////////////////////////////////////////////////////////
 
             expect(object instanceof Object).to.equal(true);
